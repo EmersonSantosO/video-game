a7b0ab597576f5c2b9e7d15a3296ab004e76b682
EmersonSantosO <emerson.santos02@inacapmail.cl>
2023-08-20 13:18:44 -0400

    Add components

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import loadPrcFile, DirectionalLight
+
+loadPrcFile("settings.prc")
 
 
 class MyGame(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
+        grassBlock = loader.loadModel("grass-block.glb")
+        grassBlock.reparentTo(render)
+        mainLight = DirectionalLight("main light")
+        mainLightNodePath = render.attachNewNode(mainLight)
+        mainLightNodePath.setHpr(30, -60, 0)
+        render.setLight(mainLightNodePath)
+
+
+game = MyGame()
+game.run()
