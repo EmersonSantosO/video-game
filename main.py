3d8e689a8bce97a33906b08698b4bd62dc43ddf4
EmersonSantosO <emerson.santos02@inacapmail.cl>
2023-08-22 12:57:14 -0400

    Add Movents

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import loadPrcFile, DirectionalLight, AmbientLight, TransparencyAttrib
+from panda3d.core import loadPrcFile, DirectionalLight, AmbientLight, TransparencyAttrib, WindowProperties, CollisionTraverser, CollisionNode, CollisionBox, CollisionTraverse, CollisionHandlerQueue
 from direct.gui.OnscreenImage import OnscreenImage
-
+from math import pi, sin, cos
 loadPrcFile("settings.prc")
+
+
+def degToRad(degrees):
+    return degrees * (pi / 180.0)
 
 
 class MyGame(ShowBase):
@@ -15,17 +19,107 @@ class MyGame(ShowBase):
         self.setupCamera()
         self.setupSkybox()
         self.captureMouse()
+        self.setupControls()
+        taskMgr.add(self.update, "update")
+
+    def update(self, task):
+        dt = globalClock.getDt()
+        playerMoveSpeed = 10
+
+        x_movement = 0
+        y_movement = 0
+        z_movement = 0
+
+        if self.keyMap['forward']:
+            x_movement -= dt * playerMoveSpeed * sin(degToRad(camera.getH()))
+            y_movement += dt * playerMoveSpeed * cos(degToRad(camera.getH()))
+        if self.keyMap['backward']:
+            x_movement += dt * playerMoveSpeed * sin(degToRad(camera.getH()))
+            y_movement -= dt * playerMoveSpeed * cos(degToRad(camera.getH()))
+        if self.keyMap['left']:
+            x_movement -= dt * playerMoveSpeed * cos(degToRad(camera.getH()))
+            y_movement -= dt * playerMoveSpeed * sin(degToRad(camera.getH()))
+        if self.keyMap['right']:
+            x_movement += dt * playerMoveSpeed * cos(degToRad(camera.getH()))
+            y_movement += dt * playerMoveSpeed * sin(degToRad(camera.getH()))
+        if self.keyMap['up']:
+            z_movement += dt * playerMoveSpeed
+        if self.keyMap['down']:
+            z_movement -= dt * playerMoveSpeed
+
+        camera.setPos(
+            camera.getX() + x_movement,
+            camera.getY() + y_movement,
+            camera.getZ() + z_movement,
+        )
+        if self.cameraSwingActivated:
+            md = self.win.getPointer(0)
+            mouseX = md.getX()
+            mouseY = md.getY()
+
+            mouseChangeX = mouseX - self.lastMouseX
+            mouseChangey = mouseY - self.lastMouseY
+
+            self.cameraSwingFactor = 10
+
+            currentH = self.camera.getH()
+            currentP = self.camera.getP()
+
+            self.camera.setHpr(
+                currentH - mouseChangeX * dt * self.cameraSwingFactor,
+                min(90, max(-90, currentP - mouseChangey *
+                    dt * self.cameraSwingFactor)), 0
+            )
+            self.lastMouseX = mouseX
+            self.lastMouseY = mouseY
+        return task.cont
 
     def setupControls(self):
+        self.keyMap = {
+            "forward": False,
+            "backward": False,
+            "left": False,
+            "right": False,
+            "up": False,
+            "down": False,
+        }
         self.accept("escape", self.relaseMouse)
+        self.accept("mouse", self.captureMouse)
+        self.accept("w", self.updateKeyMap, ["forward", True])
+        self.accept("w-up", self.updateKeyMap, ["forward", False])
+        self.accept('a', self.updateKeyMap, ['left', True])
+        self.accept('a-up', self.updateKeyMap, ['left', False])
+        self.accept('s', self.updateKeyMap, ['backward', True])
+        self.accept('s-up', self.updateKeyMap, ['backward', False])
+        self.accept('d', self.updateKeyMap, ['right', True])
+        self.accept('d-up', self.updateKeyMap, ['right', False])
+        self.accept('space', self.updateKeyMap, ['up', True])
+        self.accept('space-up', self.updateKeyMap, ['up', False])
+        self.accept('lshift', self.updateKeyMap, ['down', True])
+        self.accept('lshift-up', self.updateKeyMap, ['down', False])
+
+        self.accept('1', self.setSelectedBlockType, ['grass'])
+        self.accept('2', self.setSelectedBlockType, ['dirt'])
+        self.accept('3', self.setSelectedBlockType, ['sand'])
+        self.accept('4', self.setSelectedBlockType, ['stone'])
+
+    def updateKeyMap(self, key, value):
+        self.keyMap[key] = value
 
     def captureMouse(self):
+
+        self.cameraSwingActivated = True
+        md = self.win.getPointer(0)
+        self.lastMouseX = md.getX()
+        self.lastMouseY = md.getY()
+
         properties = WindowProperties()
         properties.setCursorHidden(True)
         properties.setMouseMode(WindowProperties.M_relative)
         self.win.requestProperties(properties)
 
     def relaseMouse(self):
+        self.cameraSwingActivated = False
         properties = WindowProperties()
         properties.setCursorHidden(False)
         properties.setMouseMode(WindowProperties.M_absolute)
